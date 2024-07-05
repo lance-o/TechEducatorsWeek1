350eae6a92fbce8e5792c9e6baeb36716f513fe9
Lawrence Cilia <cilialawrence@yahoo.co.uk>
2024-07-05 14:43:51 +0100

    add header darkening effect

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const img = document.getElementsByClassName("randomImg");
+const headerBar = document.querySelector("header");
 const randomImg = img[0];
 
 let chosenImg = "cooltower.jpg";
@@ -9,9 +10,29 @@ const mountain = "mountain.jpg";
 const someAmericanCity = "someamericancity.jpg";
 const bigVape = "bigvape.jpg";
 
+window.requestAnimationFrame(darkenBar);
+
 function getRandomNumberOneToVariable(range){
     let randomNumber = Math.floor(Math.random()*range) + 1;
     return randomNumber;
+}
+
+function reportScrollY(){
+    return window.scrollY;
+}
+
+function reportScrollYUpdate(){
+    return reportScrollY() / 1200 + 0.3;
+}
+
+function darkenBar(){
+    if(reportScrollY() >= 300){
+        headerBar.style.background = "rgba(0, 0, 0, " + `${reportScrollYUpdate()}` + ")";
+    }
+    else{
+        headerBar.style.background = "rgba(0, 0, 0, 0.1)";
+    }
+    window.requestAnimationFrame(darkenBar);
 }
 
 let randomNumber = getRandomNumberOneToVariable(5);
