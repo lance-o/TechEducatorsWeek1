57842cc9a1dda501514c7d7554b161b542eac6ef
Lawrence Cilia <cilialawrence@yahoo.co.uk>
2024-07-05 15:19:58 +0100

    comment the code (my bad)

--- scripts.js
+++ scripts.js
@@ -1,30 +1,32 @@
+//Get our attributes
 const img = document.getElementsByClassName("randomImg");
 const headerBar = document.querySelector("header");
 const randomImg = img[0];
 
+//Default image.
 let chosenImg = "cooltower.jpg";
 
+//Would have made this an array, but didn't feel like going too far ahead :)
 const coolTower = "cooltower.jpg";
 const coffee = "coffee.jpg";
 const mountain = "mountain.jpg";
 const someAmericanCity = "someamericancity.jpg";
 const bigVape = "bigvape.jpg";
 
+//Need this for darkening bar
 window.requestAnimationFrame(darkenBar);
 
-function getRandomNumberOneToVariable(range){
-    let randomNumber = Math.floor(Math.random()*range) + 1;
-    return randomNumber;
-}
-
+//Get scroll for bar darkening
 function reportScrollY(){
     return window.scrollY;
 }
 
+//Do maths on scroll for better darkening
 function reportScrollYUpdate(){
     return (reportScrollY() - 100) * (0.9 / 1270) * 2;
 }
 
+//If bar has scrolled, darken it - otherwise, it doesn't exist.
 function darkenBar(){
     if(reportScrollY() >= 100){
         headerBar.style.background = "rgba(0, 0, 0, " + `${reportScrollYUpdate()}` + ")";
@@ -35,6 +37,13 @@ function darkenBar(){
     window.requestAnimationFrame(darkenBar);
 }
 
+//Get a random number between 1 and (arg)
+function getRandomNumberOneToVariable(range){
+    let randomNumber = Math.floor(Math.random()*range) + 1;
+    return randomNumber;
+}
+
+//Load image based on what this results in
 let randomNumber = getRandomNumberOneToVariable(5);
 
 switch(randomNumber){
